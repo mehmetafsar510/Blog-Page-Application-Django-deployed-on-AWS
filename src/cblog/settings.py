ffc39e338dba9307519df52717f114cf8a0ac358
Mehmet AFŞAR <67711088+mehmetafsar510@users.noreply.github.com>
2021-05-04 23:15:04 +0300

    Update settings.py

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -145,7 +145,7 @@ LOGIN_URL = "login"
 
 AWS_STORAGE_BUCKET_NAME = 'please enter your s3 bucket name' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_REGION_NAME = "please enter your s3 region" # please enter your s3 region 
+AWS_S3_REGION_NAME = 'please enter your s3 region' # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
 
 AWS_LOCATION = 'static'
