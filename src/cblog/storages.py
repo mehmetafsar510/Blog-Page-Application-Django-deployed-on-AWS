e2ca77794c07bad865e0dd2ea8e745a54bea30f4
Mehmet AFŞAR <67711088+mehmetafsar510@users.noreply.github.com>
2022-02-28 23:06:29 +0300

    Update storages.py

--- src/cblog/storages.py
+++ src/cblog/storages.py
@@ -1,6 +1,13 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+from .settings import *
+
+# class MediaStorage(S3Boto3Storage):
+#     location = 'media'
+#     file_overwrite = False
 
 
-class MediaStore(S3Boto3Storage):
-    location = 'media'
+class PublicMediaStorage(S3Boto3Storage):
+    location = AWS_PUBLIC_MEDIA_LOCATION
     file_overwrite = False
+    default_acl = 'public-read'
+
