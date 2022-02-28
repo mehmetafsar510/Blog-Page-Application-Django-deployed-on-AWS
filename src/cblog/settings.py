ce4461c24f335dfe33a8ae3a8a070d31e82c38c2
Mehmet AFŞAR <67711088+mehmetafsar510@users.noreply.github.com>
2022-02-28 23:05:41 +0300

    Update settings.py

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -130,32 +130,44 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
-MEDIA_ROOT = BASE_DIR / "media_root"
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = "blog:list"
 
 LOGIN_URL = "login"
 
-AWS_LOCATION = 'static'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-AWS_S3_CUSTOM_DOMAIN = 'd2hozixcpfrpox.cloudfront.net'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'cblog.storages.MediaStore'
+
+
+AWS_STATIC_LOCATION = 'static'
 
 AWS_STORAGE_BUCKET_NAME = 'capstone-django'
+AWS_S3_CUSTOM_DOMAIN = 'd2hozixcpfrpox.cloudfront.net'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
+AWS_STATIC_LOCATION = 'static'
+
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
+DEFAULT_FILE_STORAGE = 'cblog.storages.PublicMediaStorage'
+
+
+MEDIA_URL = '/media/'
+
+
+# These directories comes from Dockerfile
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# with these config, we can access django admin in production due to storing CSS/HTML in static root!
+# STATIC_ROOT = '/app/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 #AWS_STORAGE_BUCKET_NAME = 'capstone-django' # please enter your s3 bucket name
 #AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 #AWS_S3_REGION_NAME = 'us-east-1' # please enter your s3 region 
