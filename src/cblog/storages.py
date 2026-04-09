d3c10be1c2bd89acf203626c58fd4b474ae49983
mehmetafsar510 <drmehmet510@gmail.com>
2026-04-09 17:31:06 +0300

    add logging

--- src/cblog/storages.py
+++ src/cblog/storages.py
@@ -5,5 +5,5 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class PublicMediaStorage(S3Boto3Storage):
     location = settings.AWS_PUBLIC_MEDIA_LOCATION
     file_overwrite = False
-    default_acl = 'public-read'
+    default_acl = None
 
